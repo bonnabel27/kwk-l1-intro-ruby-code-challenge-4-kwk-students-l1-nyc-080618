0e6b39bde78fe5ceaabdeaeb224f005f4b0d5b69
bonnabel27 <sika.attikesse@gmail.com>
2018-08-09 15:12:29 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,12 +1,6 @@
 def least_coins(x)
  money_hash = {:quarters => "0", :dimes => "0", :nickels => "0", :pennies => "0"} 
- a = x/25
-b = x%25
- c = b/10
-d = b%10
- e = d/5
-f = d%5
- g = f/10
+
 final = a + c + e + g
   puts final
 end
