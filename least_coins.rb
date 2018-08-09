fe48aa44b85b3d624d8e603a526d2628d4afc975
bonnabel27 <sika.attikesse@gmail.com>
2018-08-09 15:13:27 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,21 @@
 def least_coins(x)
  money_hash = {:quarters => "0", :dimes => "0", :nickels => "0", :pennies => "0"} 
+remainder = cents 
+  
+  quarter = cents / 25
+  remainder =  remainder % 25 
+  dime = remainder / 10
+  remainder = remainder % 10
+  nickel = remainder / 5
+  remainder = remainder % 5
+  penny = remainder / 1
+  remainder = remainder % 1
+  
+   nickel 
+  
+  return total_coins = {:quarters => quarter, :dimes => dime, :nickels => nickel, :pennies => penny}
 
-final = a + c + e + g
+final = quarter + dime + nickel + penny
   puts final
 end
 least_coins(143)
