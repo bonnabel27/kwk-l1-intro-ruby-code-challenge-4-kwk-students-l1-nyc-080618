7092a3171978b96182c2001eb3e3b73219e79211
bonnabel27 <sika.attikesse@gmail.com>
2018-08-09 15:16:31 +0000

    automatically backed up by learn

---
+++ least_coins.rb
@@ -0,0 +1,20 @@
+def least_coins(cents)
+  #Code your answer here!
+  quarters=cents/25
+  cents%=25
+  
+  dimes=cents/10
+  cents%=10
+  
+  nickels=cents/5
+  cents%=5
+  
+  pennies=cents
+  
+  coin_hash = {:quarters => quarters, :dimes => dimes, :nickels => nickels, :pennies => pennies}
+  
+  return coin_hash
+  
+end
+
+least_coins(143)
